0dbcc02e611a0ecaa3d03672f8c5216eca7a4935
SilenceZYk <15251711751@163.com>
2019-08-05 10:27:19 +0800

    topic_release

---
+++ catkin_ws/build/ros_tutorials_service/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/yjh/catkin_ws/devel/include".split(';') if "/home/yjh/catkin_ws/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "std_msgs;roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lros_tutorial_service".split(';') if "-lros_tutorial_service" != "" else []
+PROJECT_NAME = "ros_tutorials_service"
+PROJECT_SPACE_DIR = "/home/yjh/catkin_ws/devel"
+PROJECT_VERSION = "0.1.0"
---
+++ catkin_ws/src/beginner_tutorials/src/listener.cpp
@@ -0,0 +1,15 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I herad: [%s]",msg->data.c_str());
+}
+int main(int argc,char **argv)
+{
+  ros::init(argc,argv,"listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub=n.subscribe("chatter",1000,chatterCallback);
+  //int count=0;
+  ros::spin();
+  return 0;
+}
